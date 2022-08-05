66fcb6a693141a96381f5e5c1da659ae907ab74e
Namkhai B <me@forkbomb9.ch>
2022-08-05 11:01:00 -0500

    dumpling: crDroid bringup
    Signed-off-by: Namkhai B <me@forkbomb9.ch>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/dot_dumpling.mk
+    $(LOCAL_DIR)/lineage_dumpling.mk
 
 COMMON_LUNCH_CHOICES := \
-    dot_dumpling-user \
-    dot_dumpling-userdebug \
-    dot_dumpling-eng
+    lineage_dumpling-user \
+    lineage_dumpling-userdebug \
+    lineage_dumpling-eng
